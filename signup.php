eaec48d46faad6192ed0678a823830d5a591c501
fsalsayegh <as.alsayegh16@gmail.com>
2018-06-26 08:26:50 +0300

    signup page added

---
+++ signup.php
@@ -0,0 +1,25 @@
+
+
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Sign Up</title>
+</head>
+
+<body>
+	<form action="submit_signup.php" method="post">
+		<label>Username:</label>
+		<input type="text" name="username" placeholder="Enter username" id="username" required/>
+
+		<br/></br>
+
+		<label>Password:</label>
+		<input type="password" name="pass" placeholder="Enter password" id="pass" required/>
+	
+		<br/><br/>
+	
+		<input type="submit" value="submit"/>
+	</form>
+</body>
+</html>
---
+++ submit_signup.php
@@ -0,0 +1,32 @@
+<?php require_once 'Connections/pays.php'; //to connect to database very important ?> 
+<?php 
+
+	$user_name = $_POST["username"];
+	$password = $_POST["pass"];
+
+	$sql = "SELECT * FROM firsttable "; //take all data from the database
+	$result = mysql_query($sql); 
+	while($row=mysql_fetch_array($result)){ 
+	
+		 if($row['username'] == $user_name || $row['password'] == $password){
+			 echo "sorry already exists";
+		 }else{
+			mysql_query("INSERT INTO firsttable (username,password) VALUES ('$user_name','$password')");
+		//to insert data into database //we should follow the same order in database 
+			 header('Location: /fatma/list.php'); //redirect back to the list page
+		 
+		
+		 } }?>
+	
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+</body>
+	
+</html>
