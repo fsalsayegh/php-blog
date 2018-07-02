78575ea8df1c00d3d1d86e4407220ca6a9661a33
fsalsayegh <as.alsayegh16@gmail.com>
2018-07-02 13:12:10 +0300

    detail page added

---
+++ detail.php
@@ -0,0 +1,32 @@
+<!--<?php require_once'Connections/pays.php';?>-->
+
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Detail</title>
+	
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>	
+</head>
+<body>
+	<br/>
+<div class="container">
+	<div class="card text-center">
+  <div class="card-header">
+    <img   style="max-width: 70px; border-radius: 50%; top: 40px;" src="http://delo.bg/wp-content/uploads/2016/02/facebook-1.jpg" >
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  <div class="card-footer text-muted">
+    2 days ago
+  </div>
+</div>
+</body>
+</html>
